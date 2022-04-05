5f34394d208d8e11de74ac15075d63a97c69eaa8
Jay <depuytjerome@gmail.com>
2022-04-05 15:24:12 +0200

    module.css

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+// D'habitude on importe un seul module d'un seul fichier
 import Welcome, { Goodbye } from './components/welcome/welcome';
 
 function App() {
+  // les valeurs a l'intérieur des {} sont interprettées comme du code js.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>♥ Hello Mia ♥</h1>
-        <Welcome firstname="Mia" lastname="(° ͜ʖ͡°) ♥" />
-        <Goodbye firstname="Jérôme" lastname="(-_-)╭∩╮" />
+    <div className='App'>
+      <header className='App-header'>
+        <Welcome firstname=' Mia' lastname={`♥`} num={42} />
+        <img src={logo} className='App-logo' alt='logo' />
+        <Goodbye firstname='Guillaume' lastname='(° ͜ʖ͡°)╭∩╮ :' />
       </header>
     </div>
   );
--- firstapp/src/components/welcome/welcome.jsx
+++ firstapp/src/components/welcome/welcome.jsx
@@ -1,12 +1,36 @@
+import PropTypes from 'prop-types';
+import style from './welcome.module.css'
 
-// la function lamba fait d'office un return implicite :
-export const Goodbye = ({firstname, lastname}) => (<p>Goodbye {firstname} {lastname}</p>);
 export const Welcome = (props) => {
-            const {firstname, lastname} = props;
-            return (
-                <p>Bonjour {firstname} {lastname}</p>
-            );
-        };
+  const { firstname, lastname, num } = props;
+  const res = num + 2;
+  return (
+    //Fragment <></>
+    <>
+      <h1>{res} Bonjour 
+          <span className={style.name /* + ' ' + style.num */}>
+               {firstname} {lastname}
+          </span>
+      </h1>
+    </>
+  );
+};
+
+// la fonction lamba fait d'office un return implicite :
+export const Goodbye = ({ firstname, lastname, age }) => (
+  <p className={style.goodbye}>
+    Goodbye {firstname} {lastname} {age}
+  </p>
+);
+
+Goodbye.defaultProps = {
+  age: 13,
+};
+
+Welcome.propTypes = {
+  firstname: PropTypes.string.isRequired,
+  lastname: PropTypes.string,
+  num: PropTypes.number,
+};
 
 export default Welcome;
-
